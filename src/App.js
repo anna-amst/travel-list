94119e37b9d1da1cdfde81a878e62a9a16b18a60
Anna Vasileva <vasilieva.a@icloud.com>
2024-07-10 12:40:14 -0400

    add form

--- src/App.js
+++ src/App.js
@@ -23,9 +23,14 @@ function Logo() {
 
 function Form() {
   return (
-    <div className="add-form">
+    <form className="add-form">
       <h3>What do you need for your trip?</h3>
-    </div>
+      <select>
+        {Array.from({length: 20}, (_, i) => i + 1).map(num => <option value={num} key={num}>{num}</option>)}
+      </select>
+      <input type="text" placeholder="Item..."></input>
+      <button>Add</button>
+    </form>
   );
 }
 function PackingList() {
